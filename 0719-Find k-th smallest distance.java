2e1a67cdfe00d6474f20510351a0953fa50637fb
Pooja.S <spoojapooja2006@gmail.com>
2026-09-11 23:29:37 +0530

    Create 0719-Find k-th smallest distance.java

---
+++ 0719-Find k-th smallest distance.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int left = 0;
+        int right = nums[nums.length - 1] - nums[0];
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            int count = 0;
+            int j = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                while (j < nums.length && nums[j] - nums[i] <= mid)
+                    j++;
+
+                count += j - i - 1;
+            }
+
+            if (count >= k)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        return left;
+    }
+}
